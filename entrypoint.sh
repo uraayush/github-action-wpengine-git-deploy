f173a51fcf5b721593ac16db57a18100ef82afa6
Kuber Karki <105266439+kuberoutsidetech@users.noreply.github.com>
2023-04-24 15:04:19 +0545

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,7 @@ chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
 pwd
 cd ../../
+git init
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
 git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
 git push -fu $WPENGINE_ENV $BRANCH:master
