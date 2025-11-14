78c1a6cefd7548859f022106af1842c9a5e8a1ef
Aayush <aayush.shrestha@outside.tech>
2025-11-14 16:29:26 +0545

    updated remote connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,10 +28,10 @@ chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
 chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
-git config --global --add safe.directory /github/workspace
-git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
-git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
-git push -fu $WPENGINE_ENV $BRANCH:master
+# git config --global --add safe.directory /github/workspace
+# git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
+# git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
+# git push -fu $WPENGINE_ENV $BRANCH:master
 
 if [ "$ENABLE_POST_DEPLOY_SCRIPT" = "true" ]; then
     # Commands to execute if the condition is true
